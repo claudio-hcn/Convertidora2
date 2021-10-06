dc0c311f00c716af22bff7d56e5fe17c882056e8
claudio-hcn <claudio.hcn@outlook.com>
2021-10-06 20:39:25 -0300

    Initial commit

---
+++ app/src/main/java/com/example/convertidora2/Activity2.java
@@ -0,0 +1,29 @@
+package com.example.convertidora2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+       TextView txtEuros = findViewById(R.id.txtEuros);
+        txtEuros.setText(message);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/convertidora2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.convertidora2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private EditText txtPesos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtPesos=(EditText)findViewById(R.id.txtPesos);
+    }
+    public void convertir(View view){
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+        String valorPeso=txtPesos.getText().toString();
+        double pesos=Float.parseFloat(valorPeso);
+        double euros=pesos/931.54;
+        String valorEuro= decimalFormat.format(euros);
+        Intent intent = new Intent(this, Activity2.class);
+        String message = "el total en euros es \n"+valorEuro;
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
+}
